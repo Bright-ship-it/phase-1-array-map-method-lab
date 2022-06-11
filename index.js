2869250fd958fce2eb134ffa47f133343b904331
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-06-12 00:05:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -13,4 +13,14 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials
+  function titleCased(){
+    const capTutorials = tutorials.map(t => {
+      const words = t.split(" ").map(w => {
+        return w.charAt(0).toUpperCase() + w.slice(1);
+      }).join(" ");
+      console.log(words);
+      return words; 
+    })
+    return capTutorials;
+  }
 }
